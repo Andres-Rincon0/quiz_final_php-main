20062c0e36b4343910469e979da20f54390ec3da
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2023-06-17 18:17:28 -0500

    feat: :tada: inicio simulacro #1

---
+++ frontend/views/pages/products/products.php
@@ -0,0 +1,31 @@
+
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Productos</h1>
+      </div>
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Productos</a></li>
+          <li class="breadcrumb-item active">Home</li>
+        </ol>
+      </div>
+      <!-- /.card-header -->
+      
+  
+  
+      <!-- /.container-fluid -->
+    </section>
+
+      <?php
+      include "actions/list.php";
+      ?>
+    </div>
+  </div>
+
+  <!-- <button type="button" class="btn btn-primary">
+   <a href="./newProduct.php"> Agregar Producto</a>
+  </button>
+ -->
+</section>
